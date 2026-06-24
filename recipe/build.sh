a78984bf40f0e4ea464e3e746c28974979e09f3f
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-24 13:38:41 -0600

    Bump build number.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ cmake -G "Ninja" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     $SRC_DIR \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DBUILD_TESTING=OFF \
+    -DJSON_BuildTests=OFF \
     -DJSON_MultipleHeaders=ON \
     ${CMAKE_ARGS}
 ninja install
